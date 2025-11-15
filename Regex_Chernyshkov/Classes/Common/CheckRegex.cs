7cf8d910787f3bee015758cc49cc263eff628481
qwelord <secondofall22@rambler.ru>
2025-11-15 13:11:19 +0500

    Создание классов. Чернышков

---
+++ Regex_Chernyshkov/Classes/Common/CheckRegex.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Regex_Chernyshkov.Classes.Common
+{
+    public class CheckRegex
+    {
+
+        public static bool Match(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Pattern, Input);
+
+            return m.Success;
+        }
+    }
+}
---
+++ Regex_Chernyshkov/Classes/Passport.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Regex_Chernyshkov.Classes
+{
+   public class Passport
+    {
+        public string Name { get; set; }
+
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Issued {  get; set; }
+
+        public string DateOFIssued { get; set; }
+
+        public string DepartmentCode { get; set; }
+
+        public string SeriesAndNumber { get; set; }
+
+        public string DateOfBirth { get; set; }
+        
+        public string PlaceOfBirth { get; set; }
+
+
+
+
+    }
+}
